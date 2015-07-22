271fa352b1cb7cd96ff4e071116043de19f60c4a
Joshua Daghlian <josh.daghlian@nextsteplivinginc.com>
2015-07-22 14:26:33 -0400

    add soqldump.py and reasonable first cut at documentation

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # soqldump
-A simple script to dump arbitrary Salesforce SOQL query results to CSV
+A simple script to dump arbitrary Salesforce SOQL query results to CSV.
+
+# Install/config
+
+You will need Python 2.7 and the `simple_salesforce` library
+installed.  Then you'll need to put your Salesforce credentials either
+in a file called `.sfauth` (the approach I recommend) or in
+environment variables.
+
+## Installing prerequisites
+
+If you are on a Mac or Linux machine you already have python
+installed.  (You want Python 2.7.x, not Python 3.x.)  If you are on
+Windows you'll probably need to download from http://python.org and
+install as appropriate.  You need to be able to successfully say
+`python` and `pip` from a command prompt.  (The tutorial installation
+instructions at
+[https://docs.python.org/2/tutorial/interpreter.html](https://docs.python.org/2/tutorial/interpreter.html)
+show you how to add `python` to `path` environment variable; if
+`python` was in `C:\python27` then `pip` will be in
+`C:\python27\scripts`.)
+
+Once `pip` runs, install the `simple_salesforce` library:
+
+```bash
+pip install simple_salesforce
+```
+
+When everything is installed properly you should be able to run the
+following at a command prompt:
+
+```bash
+$ python
+Python 2.7.10 (default, Jun 10 2015, 19:42:47)
+[GCC 4.2.1 Compatible Apple LLVM 6.1.0 (clang-602.0.53)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import simple_salesforce
+>>>
+```
+
+If that last bit didn't throw any errors, then you have all the
+requisite software installed.
+
+Now you need to configure the Salesforce credentials that
+`soqldump.py` will use.  Edit the plain text file called `.sfauth`
+(and don't save it as `.sfauth.txt`, nor `sfauth.txt`, nor god forbid
+`sfauth.docx`) with your username, password, and security token.
+
+Restrict permissions on `.sfauth` by running `chmod 600 .sfauth` on
+Mac/Linux or whatever similar thing one should do on Windows to
+sensitive files.
+
+# Use
+
+```bash
+python soqldump.py "Id,FirstName,PhoneNumber" Account > name_and_number.csv
+```
+
+The comma separated list of column names (the "API names", not the
+human friendly names) should be surrounded by double quotes.  (No
+whitespace between the commas.)  The table (object) name probably
+doesn't need double quotes.
+
+On all three platforms, the `>` means "redirect output to a file of
+the following name", which in this example is `name_and_number.csv`.
+I strongly recommend that you embed the current date and time in the
+filename.  For example:
+
+```bash
+TS=`date +%Y_%m_%d_%H%M` python soqldump.py "Id,Name,ShoeSize"  "Shoe" > ${TS}_shoesize_and_name.csv
+```
+
+Also, I have occasionally seen this program write blank lines in its output.  If on Mac/Linux, remove those by piping the output through `sed`:
+
+```bash
+TS=`date +%Y_%m_%d_%H%M` python soqldump.py "Id,Name,ShoeSize"  "Shoe" | sed '/^\s*$/d' > ${TS}_shoesize_and_name.csv
+```
+
+# FAQ
+
+**Q:** Can't I just write "select * from SomeTable" in SOQL?
+
+**A:** Astonishingly, no!
+
+**Q:** What if I want to get a list of columns?
+
+**A:** Use
+[https://github.com/heroku/force](https://github.com/heroku/force).
+The `force` binary is actually an excellent piece of software, but I
+wrote `soqldump` because for some reason `force` doesn't have a
+version of `force query` that doesn't append `where rownum < 2001` to
+the end of your query.  It's maddening.
+
+# License
+
+Eclipse Public License v1.0 --- See LICENSE.
---
+++ soqldump.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+import os, sys
+import csv, codecs, cStringIO
+from simple_salesforce import Salesforce
+
+######################################
+##
+## Dumps Salesforce data to CSV.
+##
+## 1. To authenticate to SF, create a plain text file called .sfauth
+## (not .sfauth.txt, nor sfauth.txt, nor sfauth.docx) that has your
+## username, password, and security token.  Restrict permissions on
+## .sfauth by running "chmod 600 .sfauth" or similar.
+##
+## 2. The SOQL query we will run is defined here.  It could be any
+## SOQL query, but I'm just getting a comma separated list of columns
+## and a table name from the first two command line arguments, and
+## there's no where clause so it pulls down all the values (and not
+## just the first fifty or the first two thousand or whatever).
+##
+## 3. The actual work is done down here.  I've segregated into this
+## section all the code that probably shouldn't need to be changed
+## (except to fix bugs, in which case a pull request would be great).
+##.
+######################################
+
+
+######################################
+##
+## 1. Auth stuff
+##
+with open(".sfauth") as f:
+    for line in f:
+        k,v = line.split('=')
+        os.environ[k] = v.strip()
+username = os.environ['SFUSERNAME'] # IF YOU SEE AUTH CREDENTIALS CHECKED IN HERE THAT'S A BUG: BOTHER JOSH D
+password = os.environ['SFPASSWORD'] # IF YOU SEE AUTH CREDENTIALS CHECKED IN HERE THAT'S A BUG: BOTHER JOSH D
+security_token = os.environ['SFSECURITYTOKEN'] # IF YOU SEE AUTH CREDENTIALS CHECKED IN HERE THAT'S A BUG: BOTHER JOSH D
+
+######################################
+##
+## 2. Query stuff
+##
+
+comma_separated_column_names = sys.argv[1]
+table = sys.argv[2]
+
+soql = u"SELECT %s FROM %s"
+
+
+######################################
+##
+## 3. The implementation: Write csv to stdout
+##
+
+class UTF8Recoder:
+    """
+    Iterator that reads an encoded stream and reencodes the input to UTF-8
+    """
+    def __init__(self, f, encoding):
+        self.reader = codecs.getreader(encoding)(f)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.reader.next().encode("utf-8")
+
+class UnicodeReader:
+    """
+    A CSV reader which will iterate over lines in the CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        f = UTF8Recoder(f, encoding)
+        self.reader = csv.reader(f, dialect=dialect, **kwds)
+
+    def next(self):
+        row = self.reader.next()
+        return [unicode(s, "utf-8") for s in row]
+
+    def __iter__(self):
+        return self
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+        self.skipped_rows = 0
+
+    def writerow(self, row):
+        try:
+            self.writer.writerow([s and s.encode("utf-8") or '' for s in row])
+        except AttributeError:
+            print
+            self.skipped_rows = self.skipped_rows + 1
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+def dig_out_value(c, r, t):
+    dotIdx = c.find('.')
+    c1 = c[:dotIdx]
+    c2 = c[1+dotIdx:]
+    if dotIdx > 0 and not c1 == table:
+        res = r[c1][c2]
+    else:
+        res = r[c2]
+    if type(res) == str:
+        return res.replace('\n', '\\n').replace('\r', '\\r').strip()
+    else:
+        return res
+
+
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data), dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+
+columns = [s.strip() for s in comma_separated_column_names.split(",")]
+writer = UnicodeWriter(sys.stdout, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+writer.writerow(columns)
+printed_lines = 0
+skipped_lines = 0
+line_number = 0
+with open("failures.log", "w") as fail_log:
+
+    sf = Salesforce(username=username, password=password, security_token=security_token, sandbox=False)
+
+    result = sf.query(soql % (comma_separated_column_names, table))
+    done = False
+    while not done:
+        for r in result["records"]:
+            line_number = line_number + 1
+            # Here's where we actually print...
+            try:
+                row = [dig_out_value(c, r, table) for c in columns]
+                writer.writerow(row)
+                printed_lines = printed_lines + 1
+            except TypeError:
+                fail_log.write("# %d\n" % line_number)
+                fail_log.write(str(r))
+                fail_log.write("\n")
+                skipped_lines = skipped_lines + 1
+        if result['done']:
+            done = True
+        else:
+            result = sf.query_more(result['nextRecordsUrl'], identifier_is_url=True)
+
+if skipped_lines > 0:
+    sys.stderr.write("WARNING: Skipped %d lines.\n" % skipped_lines)
+    sys.exit(0)
+else:
+    sys.stderr.write("OK: wrote all lines successfully.\n")
+    sys.exit(0)
